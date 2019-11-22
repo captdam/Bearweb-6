2e9fa68299a6f311404e69c50099b1fc99883641
Captdam <captdam@beardle.com>
2019-11-22 05:29:45 -0500

    Create config.php

---
+++ webserver/config.php
@@ -0,0 +1,22 @@
+<?php
+	//Overall config
+	define('URL_PARAM',	'url');
+	
+	//Database login
+	define('DB_HOST',	'localhost');
+	define('DB_USERNAME',	'bearweb');
+	define('DB_PASSWORD',	'x');
+	define('DB_NAME',	'Bearweb');
+	
+	//Object storgae
+	define('OS_USERNAME',	'x');
+	define('OS_PASSWORD',	'x');
+	define('OS_TENANT',	'x');
+	define('OS_CONTAINER',	'Bearweb');
+	
+	//Security
+	define('FORCEHTTPS',	true);
+	
+	//Debug mode
+	define('DEBUGMODE',	true);
+?>
