8dff0f376e822ded10bf2cf07d13e8124ee4c8a8
Captdam <captdam@beardle.com>
2020-03-14 21:45:01 -0400

    Add script: Generate XML sitemap

--- webserver/config.php
+++ webserver/config.php
@@ -18,6 +18,12 @@
 	//Security
 	define('FORCEHTTPS',	true);
 	
+	//SEO
+	define('SEO_DOMAIN', array( #When generate sitemap, URLs will be this + url_of_resources. echo: SEO_DOMAIN[SITENAME]."local/url/to/resource.html"
+		'captdam.com' => 'https://captdam.com/',
+		'beardle.com' => 'https://beardle.com/'
+	));
+	
 	//Debug mode
 	define('DEBUGMODE',	true);
 ?>
