00b37c0fdfb406d69484f09719aeed45a51d708d
Captdam <captdam@beardle.com>
2020-10-08 02:01:41 -0400

    Fix object_externalimage template

--- webserver/template/object_externalimage.php
+++ webserver/template/object_externalimage.php
@@ -2,10 +2,14 @@
 	//Show HD from storage server
 	if (array_key_exists('HD',$_GET)) {
 		$os = 'https://object-storage.tyo1.conoha.io/v1/nc_'.OS_TENANT.'/'.
-			OS_PUBLICCONTAINER.'/'.SITENAME.'/'.$PAGEDATA['URL'];
+			OS_PUBLICCONTAINER.'/'.SITENAME.'/'.$object['URL'];
 		http_response_code(303);
 		header('Location: '.$os);
 		$BW->log('Redirect to orginal image at: '.$os);
+
+		//In case 303 is not supported
+		header('Content-Type: text/html');
+		echo '<html><h1>Orginal image on object storage server</h1><p><a href="'.$os.'">Click this click if your browser is not automatically redirected to the external server</a></p></html>';
 	}
 	
 	//Show thumb from local
