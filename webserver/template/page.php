85386a85385511c6bb0a94f73edc4ccf2525973c
Captdam <captdam@beardle.com>
2020-10-19 01:02:40 -0400

    New webpage template and config

--- webserver/template/page.php
+++ webserver/template/page.php
@@ -1,103 +1,83 @@
 <?php
-	//MIME
+	$BW->log('Execute page template.');
 	header('Content-Type: text/html');
 	
 	//Get config
-	writeLog('Read page template config file.');
-	$filename = './template/'.SITENAME.'_page.json';
-	$pageConfig = json_decode(file_get_contents($filename),true);
+	try {
+		$BW->log('Reading page template config file.');
+		$filename = './template/'.SITENAME.'_page.json';
+		$pageConfig = json_decode(file_get_contents($filename), true);
+	} catch (Exception $e) {
+		throw BW_WebServerError(500, 'Fail to load config file: '.$e->getMessage());
+	}
 	
-	//Get all pages with the given sitename and URL
-	writeLog('Fetch page index.');
-	$pageIndex = $this->database->call(
-		'Webpage_getLanguageIndex',
-		array(
-			'Site'	=> $BW->page['Site'],
-			'URL'	=> $BW->page['URL']
-		),
-	true);
-	if (!$pageIndex)
-		throw new BW_ClientError(404,'Page not found.');
-	
-	foreach ($pageIndex as &$x)
-		$x = $x['Language'];
+	//Get all pages with different multilingual info by the given sitename and URL
+	$BW->log('Fetch page language index.');
+	$pageIndex = $BW->query('BW_Webpage_getLanguageIndex', [ $BW->sitemap['Site'], $BW->sitemap['URL'] ], 2);
+	if (!$pageIndex) #Record found in BW_Sitemap but missing in BW_Webpage
+		throw new BW_DatabaseServerError(404, 'Webpage not found.');
+
+	foreach ($pageIndex as &$x)  #$pageIndex = [0=>['Language'=>lang1], 1=>['Language'=>lang2], 2=>['Language'=>lang3], ...]
+		$x = $x['Language']; #$pageIndex = [lang1, lang2, lang3, ...]
 	unset($x);
+	$language = selectMultilingual($pageIndex, $BW->language, $BW->region, $pageConfig['DefaultLanguage'], $pageConfig['DefaultRegion']);
 	
-	
-	//Multilingual page content
-	writeLog('Page index size (multilingual): '.count($pageIndex));
-	$userLocation = trim($BW->location['language'].'-'.$BW->location['region'],'-');
-	$language = chooseLanguage($pageIndex,$userLocation,$pageConfig['DefaultLanguage']);
-	
-	//Fetch webpage data
-	writeLog('Fetch page: ('.$BW->page['Site'].') - ('.$language.') - '.$BW->URL);
-	$webpage = $this->database->call(
-		'Webpage_get',
-		array(
-			'Site'		=> $BW->page['Site'],
-			'URL'		=> $BW->URL,
-			'Language'	=> $language
-		),
-	true)[0];
+	//Fetch webpage data by specific multilingual info
+	$BW->log('Fetch webpage: Site('.$BW->sitemap['Site'].') Language('.$language.') URL('.$BW->sitemap['URL'].').');
+	$webpage = $BW->query('BW_Webpage_get', [ $BW->sitemap['Site'], $BW->sitemap['URL'], $language ], 1);
 	
 
 /********************************* TO WRITE THE TEMPLATE *********************************/
 
-	//This contains page data from BW_Sitemap and BW_Webpage
-	$PAGEDATA = array_merge($webpage,$BW->page);
+	//Data from database BW_Sitemap and BW_Webpage
+	$BW_SITEMAP = $BW->sitemap;
+	$BW_WEBPAGE = $webpage;
 	
 	//Prefix user's multilingual info to links on the page
-	$USERLANGUAGE = $userLocation=='' ? '' : ('/'.$userLocation);
+	$BW_URLPREFIX = ( trim($BW->language.'-'.$BW->region,'-') == '' ) ? '' : ('/'.trim($BW->language.'-'.$BW->region,'-'));
 	
-	//Array of all available language for this page (Get `Language` from `BW_Webpage` by `URL`)
-	$LANGUAGESET = $pageIndex;
+	//Array of all available language for this page (SELECT `Language` FROM `BW_Webpage` WHERE `Site+URL`)
+	$BW_MULTILINGUAL = $pageIndex;
 	
 	//Webpage template data in JSON saved in extra file
-	$TEMPLATEDATA = $pageConfig;
-	
-	/*
-	Multilingual in template:
-	To support multilingual in template, write HTML code in this way:
-	<?php if ($USERLANGUAGE == 'option-language1'): ?> #Language + Region
-		<span>Content in option language 1</span>
-	<?php elseif (substr($USERLANGUAGE,1,2) == 'option-language2'): ?> #Language
-		<span>Content in option language 2</span>
-	<?php else: ?>
-		<span>Content in default language</span>
-	<?php endif; ?>
-	Do not forget $BW->site['DefaultLanguage'] in stored BW_Config.
-	*/
-	
-	//Language used on the page (Those languages are available in the SITENAME_page.json)
-	if (substr($USERLANGUAGE,1,2) == 'en')
-		$PAGELANG = 'en';
-	else
-		$PAGELANG = 'default';
+	$BW_TEMPLATEDATA = $pageConfig;
 
 /*****************************************************************************************/
+
+	//Text used in template
+	$BW_TEMPLATE_TEXT = array(
+		'__INDEX__' => ['en','zh'],
+		'SEARCH_BOX' => ['Enter keyword to search','输入关键字搜索'],
+		'FOOTER_MULTILINGUAL_PICKUP' => ['This page is also avaliable in','本页面亦适用于这些语言'],
+		'MODAL_CLOSE' => ['Close','关闭']
+	);
+	$BW_TEMPLATE_TEXT = multilingualTextFilter($BW_TEMPLATE_TEXT, $BW->language, $BW->region, $BW_TEMPLATEDATA['DefaultLanguage'], $BW_TEMPLATEDATA['DefaultRegion']);
+	$BW_NAV = multilingualTextFilter($BW_TEMPLATEDATA['Nav'], $BW->language, $BW->region, $BW_TEMPLATEDATA['DefaultLanguage'], $BW_TEMPLATEDATA['DefaultRegion']);
+
 ?>
 <!DOCTYPE html>
 <html
-	data-pagestatus="<?= $PAGEDATA['Status'] ?>"
+	data-pagestatus="<?= $BW_SITEMAP['Status'] ?>"
 	data-httpstatus="<?= http_response_code() ?>"
-	lang="<?= $PAGEDATA['Language'] ?>"
-	data-urlprefix="<?= $USERLANGUAGE ?>"
+	lang="<?= $BW_WEBPAGE['Language'] ?>"
+	data-urlprefix="<?= $BW_URLPREFIX ?>"
 >
 	<head>
-		<title><?= $PAGEDATA['Title'].' - '.SITENAME; ?></title>
-		<meta name="keywords" content="<?= $PAGEDATA['Keywords'] ?>" />
-		<meta name="description" content="<?= $PAGEDATA['Description'] ?>" />
-		<meta name="author" content="<?= $PAGEDATA['Author'] ?>" />
-		<meta name="robots" content="<?= $PAGEDATA['Status'] == 'S' ? 'noindex' : 'index'; ?>" />
+		<title><?= $BW_WEBPAGE['Title'].' - '.SITENAME; ?></title>
+		<meta name="keywords" content="<?= $BW_WEBPAGE['Keywords'] ?>" />
+		<meta name="description" content="<?= $BW_WEBPAGE['Description'] ?>" />
+		<meta name="author" content="<?= $BW_SITEMAP['Author'] ?>" />
+		<meta name="robots" content="<?= $BW_SITEMAP['Status'] == 'S' ? 'noindex' : 'index'; ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<meta charset="utf-8" />
 		<link href="/web/favorite.png" rel="icon" type="image/png" />
 		<link href="/web/style.css" rel="stylesheet" type="text/css" />
 		<link href="/web/stylemodules.css" rel="stylesheet" type="text/css" />
 <?php
-	//If "S", SEO no index, do not provide multilingual info
-	if ($PAGEDATA['Status'] != 'S') foreach ($LANGUAGESET as $x)
-		echo '<link rel="alternate" hreflang="',$x,'" href="/',$x,'/',$PAGEDATA['URL'],'" />';
+	if ($BW_SITEMAP['Status'] != 'S') { #If "S" (SEO no index), do not provide multilingual info
+		foreach ($BW_MULTILINGUAL as $x)
+			echo '<link rel="alternate" hreflang="',$x,'" href="/',$x,'/',$BW_SITEMAP['URL'],'" />';
+	}
 ?>
 		<script src="/web/ajax.js"></script>
 		<script src="/web/md5.js"></script>
@@ -106,19 +86,15 @@
 	</head>
 	<body>
 		<header>
-			<h1 id="header_logo">Beardle</h1>
+			<h1 id="header_logo"><?= $BW_TEMPLATEDATA['Name'] ?></h1>
 			<span id="phone_menu_button">≡</span>
-			<form id="search_container" action="<?= $USERLANGUAGE ?>/search" method="get" target="searchtab">
-<?php if ($PAGELANG == 'en'): ?>
-				<input name="search" id="search" placeholder="Enter keyword to search" />
-<?php else: ?>
-				<input name="search" id="search" placeholder="输入关键字搜索" />
-<?php endif; ?>
+			<form id="search_container" action="<?= $BW_URLPREFIX ?>/search" method="get" target="searchtab">
+				<input name="search" id="search" placeholder="<?= $BW_TEMPLATE_TEXT['SEARCH_BOX'] ?>" />
 			</form>
 			<nav id="header_nav">
 <?php
-	foreach ($TEMPLATEDATA['NavLinks'][$PAGELANG] as $name=>$link)
-		echo '<a href="',$USERLANGUAGE,$link,'">',$name,'</a>';
+	foreach ($BW_NAV as $url=>$text)
+		echo '<a href="',$BW_URLPREFIX,$url,'">',$text,'</a>';
 ?>
 			</nav>
 		</header>
@@ -126,10 +102,10 @@
 			<img src="/web/top.png" alt="Top of page" title="To page top" />
 		</div>
 		<main>
-			<div id="main_title" style="background-image:url('/<?= $PAGEDATA['Info']['poster'] ?? 'web/banner.jpg' ?>')">
+			<div id="main_title" style="background-image:url('/<?= $BW_SITEMAP['Info']['poster'] ?? 'web/banner.jpg' ?>')">
 				<div>
-					<h1><?= $PAGEDATA['Title']; ?></h1>
-					<p><?= $PAGEDATA['Description']; ?></p>
+					<h1><?= $BW_WEBPAGE['Title']; ?></h1>
+					<p><?= $BW_WEBPAGE['Description']; ?></p>
 				</div>
 			</div>
 			<div id="main_content">
@@ -141,50 +117,33 @@
 			<div class="pltr">
 				<img src="/web/logo.png" />
 				<div>
-					<p>Admin: <?= $TEMPLATEDATA['SiteOwner'][$PAGELANG] ?></p>
-					<p>Admin e-mail: <a href="mailto:<?= $TEMPLATEDATA['AdminEmail'] ?>"><?= $TEMPLATEDATA['AdminEmail'] ?></a></p>
+					<p>Admin: <?= $BW_TEMPLATEDATA['SiteOwner'] ?></p>
+					<p>Admin e-mail: <a href="mailto:<?= $BW_TEMPLATEDATA['AdminEmail'] ?>"><?= $BW_TEMPLATEDATA['AdminEmail'] ?></a></p>
 					<p>© 
 <?php
-	$cprAuthorField = $PAGEDATA['Author'] ? '<span class="bearweb_author">'.$PAGEDATA['Author'].'</span>' : '';
-	if ($PAGELANG == 'en') {
-		if (!$PAGEDATA['Copyright'])
-			echo trim( $cprAuthorField.' - All rights reserved' ," \-");
-		else if (substr($PAGEDATA['Copyright'],0,2) == 'R=')
-			echo trim( substr($PAGEDATA['Copyright'],2).' - Uploaded by: '.$cprAuthorField ," \-");
-		else
-			echo trim( $cprAuthorField.' ★ This work is licensed under '.$PAGEDATA['Copyright'] ," \-");
-	}
-	else {
-		if (!$PAGEDATA['Copyright'])
-			echo trim( $cprAuthorField.' - 保留一切权利' ," \-");
-		else if (substr($PAGEDATA['Copyright'],0,2) == 'R=')
-			echo trim( substr($PAGEDATA['Copyright'],2).' - 由'.$cprAuthorField.'上传' ," \-");
-		else
-			echo trim( $cprAuthorField.' ★ 按照'.$PAGEDATA['Copyright'].'协议进行共享' ," \-");
-	}
+	$cprAuthorField = $BW_SITEMAP['Author'] ? '<span class="bearweb_author">'.$BW_SITEMAP['Author'].'</span>' : '';
+	echo $cprAuthorField;
+/*	if (!$PAGEDATA['Copyright'])
+		echo trim( $cprAuthorField.' - '.$BW_TEMPLATE_TEXT['ALL_RIGHTS_RESERVED'] ," \-");
+	else if (substr($PAGEDATA['Copyright'],0,2) == 'R=')
+		echo trim( substr($PAGEDATA['Copyright'],2).' - Uploaded by: '.$cprAuthorField ," \-");
+	else
+		echo trim( $cprAuthorField.' ★ This work is licensed under '.$PAGEDATA['Copyright'] ," \-");*/
 ?>
 					</p>
 				</div>
 			</div>
 			<div>
 <?php
-	switch ($PAGELANG) {
-		case 'en':	echo '<span>This page is also avaliable in: </span>'; break;
-		default:	echo '<span>本页面亦适用于这些语言： </span>';
-	}
-	foreach ($LANGUAGESET as $x)
-		echo '<a class="langlink" hreflang="',$x,'" href="/',$x,'/',$PAGEDATA['URL'],'">',$x,'</a> ';
+	echo '<span>',$BW_TEMPLATE_TEXT['FOOTER_MULTILINGUAL_PICKUP'],': </span>';
+	foreach ($BW_MULTILINGUAL as $x)
+		echo '<a class="langlink" hreflang="',$x,'" href="/',$x,'/',$BW->url,'">',$x,'</a> ';
 ?>
 			</div>
 		</footer>
 		
 		<div id="modal_container" onclick="modal()"><div id="modal">
-			
-<?php if ($PAGELANG == 'en'): ?>
-			<div id="modal_close">╳ （Close）</div>
-<?php else: ?>
-			<div id="modal_close">╳ （关闭）</div>
-<?php endif; ?>
+			<div id="modal_close">╳ <?= $BW_TEMPLATE_TEXT['MODAL_CLOSE'] ?></div>
 			<div id="modal_content" onclick="event.stopPropagation()"></div>
 		</div></div>
 	</body>
